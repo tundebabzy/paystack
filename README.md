4e13d02647328c1ff43ae5d1f2b84937ec625fa0
tundebabzy <tundebabzy@gmail.com>
2018-05-19 14:14:04 +0100

    initial commit with `Paystack Settings` doctype

---
+++ README.md
@@ -0,0 +1,7 @@
+## Paystack
+
+App that integrates Paystack into ERPNext
+
+#### License
+
+MIT
---
+++ paystack/paystack/doctype/paystack_settings/paystack_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, XLevel Retail Systems Nigeria Ltd and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Paystack Settings', {
+	refresh: function(frm) {
+
+	}
+});
