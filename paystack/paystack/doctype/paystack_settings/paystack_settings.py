23a2f4f794716a093c04e4779a1eb813a8fb3b1b
tundebabzy <tundebabzy@gmail.com>
2018-05-20 14:51:50 +0100

    add validation

--- paystack/paystack/doctype/paystack_settings/paystack_settings.py
+++ paystack/paystack/doctype/paystack_settings/paystack_settings.py
@@ -3,8 +3,32 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+from frappe import _
 import frappe
+from frappe.integrations.utils import create_payment_gateway
 from frappe.model.document import Document
+from frappe.utils import call_hook_method
+from requests import RequestException
+
+
+SUPPORTED_CURRENCIES = ['NGN']
+
 
 class PaystackSettings(Document):
-	pass
+	def validate(self):
+		create_payment_gateway("Paystack")
+		call_hook_method('payment_gateway_enabled', gateway="Paystack")
+		if not self.flags.ignore_mandatory:
+			self.validate_credentials()
+
+	def validate_credentials(self):
+		import paystakk
+		try:
+			paystack = paystakk.TransferControl(secret_key=self.secret_key, public_key=self.public_key)
+			paystack.get_balance()
+		except RequestException:
+			frappe.throw(
+				_('Verification of your credentials failed. Please ensure you '
+				  'have a working internet connection and that your '
+				  'credentials are correct')
+			)
